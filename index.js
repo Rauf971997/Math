e7d50e5eea004235b60aec61511d7b38d91f2411
Baghirov Rauf <rauf777bagirov7@gmail.com>
2023-11-02 21:13:40 +0400

    Added new homework

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function myMath(operation, ...numbers) {
+/* function myMath(operation, ...numbers) {
     let result = 0;
   
     if (operation in Math && typeof Math[operation] === 'function') {
@@ -67,4 +67,63 @@ function myMath(operation, ...numbers) {
   console.log(myMath("subtract"));
   console.log(myMath("divide"));
   console.log(myMath("multiply"));
-  console.log(myMath("DiVide",5,0));
+  console.log(myMath("DiVide",5,0)); */
+
+
+  function MyArray(...args) {
+    this.elements = {
+        length: 0
+    };
+
+    this.push = function(...args) {
+        for (const arg of args) {
+            const index = this.elements.length;
+            this.elements[index] = arg;
+            ++this.elements.length;
+        }
+        return this.elements.length;
+    };
+
+    this.shift = function() {
+        if (this.elements.length === 0) {
+            return undefined;
+        }
+
+        const firstElement = this.elements[0];
+
+        for (let i = 0; i < this.elements.length - 1; i++) {
+            this.elements[i] = this.elements[i + 1];
+        }
+
+        delete this.elements[this.elements.length - 1];
+        --this.elements.length;
+
+        return firstElement;
+    };
+
+    this.unshift = function(...args) {
+        const argsLength = args.length;
+        for (let i = this.elements.length - 1; i >= 0; i--) {
+            this.elements[i + argsLength] = this.elements[i];
+        }
+        for (let i = 0; i < args.length; i++) {
+            this.elements[i] = args[i];
+        }
+        this.elements.length += argsLength;
+        return this.elements.length;
+    };
+
+    this.push(...args);
+}
+
+
+
+
+const arr = new MyArray(1, '2', 3,'Step');
+
+arr.unshift('Rauf', 233); 
+console.table(arr.elements);
+
+
+arr.shift();
+console.table(arr.elements);
