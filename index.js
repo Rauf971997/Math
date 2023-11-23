0a2798c2d074fafb3962bccb32b5832a440f1448
Baghirov Rauf <rauf777bagirov7@gmail.com>
2023-11-23 18:43:18 +0400

    added new homework

--- index.js
+++ index.js
@@ -1,88 +1,19 @@
-class Human{
-    #name;
-    #age;
-    constructor(name,age){
-        this.#name=name;
-        this.#age=age;  
-    }
+function createTable(n, m) {
+    let table = document.createElement('table');
 
-    get name(){
-        return this.#name;
-    }
-
-    get age(){
-        return this.#age;
-    }
-    
-}
-
-
-class Player extends Human{
-     #sportName;
-     #inMainTeam;
-    constructor(name, age, sportName, inMainTeam){
-      super(name,age);
-        this.#sportName=sportName;
-        this.#inMainTeam=inMainTeam;
-    }
-
-    get sportName() {
-        return this.#sportName;
-    }
-
-    get inMainTeam() {
-        return this.#inMainTeam;
-    }
-
-}
-
-
-class FootbalPlayer extends Human{
-    constructor(name, age, inMainTeam){
-        super(name, age, 'MMA', inMainTeam);
-    }
-}
-
-
-class Team {
-    #footballPlayers;
-
-    constructor() {
-        this.#footballPlayers = [];
-    }
-
-    get players() {
-        return [...this.#footballPlayers];
-    }
-
-    addPlayers(...footballPlayers) {
-        this.#footballPlayers.push(...footballPlayers);
-    }
-
-    deletePlayer(footballPlayer) {
-        const indexOfFootballPlayer = this.#footballPlayers.indexOf(footballPlayer);
-
-        if (indexOfFootballPlayer !== -1) {
-            this.#footballPlayers.splice(indexOfFootballPlayer, 1);
+    for (let i = 0; i < n; i++) {
+        let row = table.insertRow();
+        for (let j = 0; j < m; j++) {
+            let cell = row.insertCell();
+            cell.textContent = `Ячейка ${i + 1}-${j + 1}`;
         }
     }
 
-    getPlayersSortedByName() {
-        return [...this.#footballPlayers].sort(function(a, b) {
-            const name1 = a.name.toLowerCase();
-            const name2 = b.name.toLowerCase();
-
-            if (name1 > name2) return 1;
-            if (name1 < name2) return -1;
-
-            return 0;
-        });
-    }
-
-    getFilteredPlayers(inMainTeam) {
-        return this.#footballPlayers.filter(function(footballPlayer) {
-            return footballPlayer.inMainTeam === inMainTeam;
-        });
-    }
+    return table;
 }
 
+
+
+
+let generatedTable = createTable(2, 5);
+document.body.appendChild(generatedTable);
