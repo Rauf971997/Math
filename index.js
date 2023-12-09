541fbdd361db381a11e4f3c60469882b56990eed
Baghirov Rauf <rauf777bagirov7@gmail.com>
2023-12-09 18:12:48 +0400

    Added my two homework

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
-function createTable(n, m) {
-    let table = document.createElement('table');
+const links = document.querySelectorAll("a");
+const socialLinks = document.querySelector("#socialLinks");
 
-    for (let i = 0; i < n; i++) {
-        let row = table.insertRow();
-        for (let j = 0; j < m; j++) {
-            let cell = row.insertCell();
-            cell.textContent = `Ячейка ${i + 1}-${j + 1}`;
-        }
-    }
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+}
 
-    return table;
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("mouseenter", function () {
+    const randomColor = getRandomColor();
+    this.style.color = randomColor;
+  });
+
+  links[i].addEventListener("mouseleave", function () {
+    this.style.color = "";
+  });
+}
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 
 
+// 2-ое задание 
+const button = document.querySelector('button');
 
-let generatedTable = createTable(2, 5);
-document.body.appendChild(generatedTable);
+function showMessage() {
+  const userInput = prompt('Введите число от 1 до 10:');
+
+  if (userInput >= 1 && userInput <= 10) {
+    for (let i = 1; i <= userInput; i++) {
+      console.log(i);
+    }
+  }
+}
+
+button.addEventListener('click', showMessage);
